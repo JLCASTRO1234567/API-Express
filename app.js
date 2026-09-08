577632e2147399915a4f9822b6643f3004f9235c
JLCASTRO1234567 <jlcastrorodrigues@gmail.com>
2026-09-08 01:00:13 +0000

    atividade 1 & 2

--- app.js
+++ app.js
@@ -35,6 +35,22 @@ app.get('/usuario/:id', (req,res) => {
     res.json(usuario);
 });
 
+app.get('/produtos/busca', (req, res) => {
+    const { categoria } = req.query
+
+    const resultadoCategoria = produtos.filter((produtos) => produtos.categoria == categoria)
+    return res.json(resultadoCategoria)
+});
+
+app.get('/produtos/:id', (req, res) => {
+    const { id } = req.params;
+    const produto = produtos.find( u => u.id === parseInt(id));
+    if (!produto) {
+        return res.status(404).json({ erro: 'Produto não encontrado'});
+    }
+     res.json(produto)
+});
+
 app.listen(PORT, () => {
     console.log(`servidor rodando em http://localhost:${PORT}`)
 });
